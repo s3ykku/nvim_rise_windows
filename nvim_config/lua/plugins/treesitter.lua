e42cff9ea686f321e1b278f5393912439f511c03
Roman <s3ykku@gmail.com>
2026-02-07 23:56:46 +0300

    changed tab logic for python files

--- nvim_config/lua/plugins/treesitter.lua
+++ nvim_config/lua/plugins/treesitter.lua
@@ -1,16 +1,21 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    init = function()
-        -- Оставляем только самое важное для Windows
-        local install = require("nvim-treesitter.install")
-        install.compilers = { "cl", "gcc" } -- Чтобы он сразу шел к нашим shim-файлам
-        install.prefer_git = false          -- Чтобы не мучиться с git-блоками на Windows
-    end,
-    config = function()
-        require("nvim-treesitter").setup({
-            highlight = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	init = function()
+		-- Оставляем только самое важное для Windows
+		local install = require("nvim-treesitter.install")
+		install.compilers = { "cl", "gcc" } -- Чтобы он сразу шел к нашим shim-файлам
+		install.prefer_git = false -- Чтобы не мучиться с git-блоками на Windows
+	end,
+	config = function()
+		require("nvim-treesitter").setup({
+			highlight = { enable = true },
+		})
+		require("nvim-treesitter.config").setup({
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
