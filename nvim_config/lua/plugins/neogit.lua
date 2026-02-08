63c662119dbdfc1fccec8f6f1ad4ce6f376e66f2
s3ykku <s3ykku@gmail.com>
2026-02-08 13:19:59 +0300

    added neogit plugin (tui git in neovim, better then in vscode :) )

---
+++ nvim_config/lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+	"NeogitOrg/neogit",
+	lazy = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+	},
+	cmd = "Neogit",
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+	},
+}
